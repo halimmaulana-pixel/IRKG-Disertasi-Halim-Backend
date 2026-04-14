69fc0b34eb1a98b1dfd0176de504e4044baa62ee
Halim Maulana <halimmaulana@umsu.ac.id>
2026-04-14 16:41:10 +0700

    Simple working

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
-# Simple test
-from fastapi import FastAPI, APIRouter
+# Simple working test
+from fastapi import FastAPI
 
 app = FastAPI()
-
-# Without try-except to see error
-import routers.graph
-
-router = APIRouter()
-
-
-@router.get("/test")
-def test():
-    return {"ok": True}
-
-
-app.include_router(router, prefix="/api/graph")
 
 
 @app.get("/")
 def root():
-    return {"status": "OK"}
+    return {"status": "OK", "message": "Backend working!"}
 
 
 @app.get("/health")
